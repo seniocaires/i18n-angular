0b8e5f54bdd5383954036e16858499f3632d5903
Senio Caires <seniocaires@gmail.com>
2015-11-03 08:05:36 -0200

    Update i18n-angular.js

--- js/i18n-angular.js
+++ js/i18n-angular.js
@@ -1,3 +1,15 @@
+/**
+ * i18n-angular
+ * Internacionalização (i18n) com Angular usando arquivos de mensagens JSON
+ * 
+ * Repositório
+ * https://github.com/seniocaires/i18n-angular
+ * 
+ * Os commits são publicados automaticamente na página
+ * http://seniocaires.github.io/i18n-angular
+ * 
+ * @author Senio Caires
+ */
 var app = angular.module('i18nApp', []);
 
 app.controller('i18nController', function($scope, $http) {
